1b9df95c018808e1768ef0e66bc4682b198eaec8
Oleg <smvzhgt@gmail.com>
2020-05-30 10:53:16 +0300

    init commit

---
+++ Nytapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nytapp
+//
+//  Created by oelg on 30.05.2020.
+//  Copyright © 2020 Nytapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
